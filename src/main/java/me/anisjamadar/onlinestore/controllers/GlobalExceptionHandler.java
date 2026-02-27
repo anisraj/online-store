5ad0f5261e15605498a0b14378229c071bc03bc2
anisraj <anisjamadar26@gmail.com>
2026-02-27 10:42:23 +0530

    handling invalid request body error

--- src/main/java/me/anisjamadar/onlinestore/controllers/GlobalExceptionHandler.java
+++ src/main/java/me/anisjamadar/onlinestore/controllers/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package me.anisjamadar.onlinestore.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,5 +20,12 @@ public class GlobalExceptionHandler {
             errors.put(error.getField(), error.getDefaultMessage());
         });
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleUnreadableMessage() {
+        return ResponseEntity.badRequest().body(
+            Map.of("error", "Invalid request body")
+        );
     }
 }
